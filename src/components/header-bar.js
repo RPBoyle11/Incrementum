76f45e79a31ab2a18269f8e592cd32f8a84abbf8
R-Cole <randy@goodglue.com>
2019-02-01 14:03:57 -0700

    readme images

--- src/components/header-bar.js
+++ src/components/header-bar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {clearAuth} from '../actions/auth';
 import {clearAuthToken} from '../local-storage';
-import italian5000 from '../images/header_stuff_01.jpg';
+import italian5000 from '../images/header_stuff_01.png';
 
 export class HeaderBar extends React.Component {
     logOut() {
